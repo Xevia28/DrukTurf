69d8337ddee92854d22f4358506f3bae388369db
xeviabcd28 <xeviabcd28@gmail.com>
2025-05-30 22:37:36 +0600

    push user side static

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#04374C',
+                secondary: '#06B6D4',
+                accent: '#10B981',
+                dark: '#0F172A',
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        }
+    }
+}
