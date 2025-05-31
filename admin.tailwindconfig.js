f324d99e02e809c3f19c3c6fd773f6ff816f8c7e
xeviabcd28 <xeviabcd28@gmail.com>
2025-05-31 13:10:32 +0600

    updated admin side

---
+++ admin.tailwindconfig.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#04374C',
+                'secondary': '#04A0D3',
+                'accent': '#00C9A7',
+                'dark': '#121826',
+                'turf-green': '#1DB954'
+            }
+        }
+    }
+}
